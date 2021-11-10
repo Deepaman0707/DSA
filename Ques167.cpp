371960d251bc50c6020b44cb93d2bfa78cbcf6c9
Aman Deep <deep.aman07.ad@gmail.com>
2021-11-11 03:36:52 +0530

    .

---
+++ Ques167.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define all(a) (a).begin(), (a).end()
+#define ff first
+#define ss second
+#define ll long long int
+#define fast ios_base::sync_with_stdio(NULL), cin.tie(0), cout.tie(0)
+#define MOD 1e9 + 7
+#define cin(a, n)               \
+    for (int i = 0; i < n; i++) \
+    cin >> a[i]
+
+#define cout(a, n)              \
+    for (int i = 0; i < n; i++) \
+    cout << a[i]
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define forr(i, n) for (int i = n; i >= 0; i--)
+#define fep(i, a, b) for (int i = a; i < b; i++)
+
+struct tree
+{
+    int val;
+    tree *left;
+    tree *right;
+    tree(int value)
+    {
+        val = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Recursive method for inorder traversal.
+
+void inOrderTraversal(tree *node)
+{
+    if (node == NULL)
+        return;
+    inOrderTraversal(node->left);
+    cout << node->val << " ";
+    inOrderTraversal(node->right);
+}
+
+void solve()
+{
+    tree *root = new tree(1);
+    root->left = new tree(2);
+    root->right = new tree(3);
+    root->left->left = new tree(4);
+    root->left->right = new tree(5);
+    root->right->left = new tree(6);
+    root->right->right = new tree(7);
+
+    inOrderTraversal(root);
+}
+
+int main()
+{
+    fast;
+    solve();
+    return 0;
+}
