f4d8ae86385a05253997cd9a321301b2cd8fa6fa
Aman Deep <deep.aman07.ad@gmail.com>
2021-11-21 15:19:17 +0530

    .

---
+++ Ques23.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define all(a) (a).begin(), (a).end()
+#define ff first
+#define ss second
+#define ll long long int
+#define fast ios_base::sync_with_stdio(NULL), cin.tie(0), cout.tie(0)
+#define MOD 1e9 + 7
+#define cin(a, n)               \
+    for (int i = 0; i < n; i++) \
+    cin >> a[i]
+
+#define cout(a, n)              \
+    for (int i = 0; i < n; i++) \
+    cout << a[i]
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define forr(i, n) for (int i = n; i >= 0; i--)
+#define fep(i, a, b) for (int i = a; i < b; i++)
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    cin(a, n);
+    int maxvalue = a[0], minvalue = a[0];
+    int ans = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < 0)
+            swap(maxvalue, minvalue);
+
+        maxvalue = max(a[i], maxvalue * a[i]);
+        minvalue = min(a[i], minvalue * a[i]);
+
+        ans = max(maxvalue, ans);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+    solve();
+    return 0;
+}
